90b33c141c3413dd8ea8824e26ccc61a14171fa6
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-10-10 21:29:27 +0900

    add feat

--- react-budget-app/src/components/ExpenseForm.js
+++ react-budget-app/src/components/ExpenseForm.js
@@ -1,39 +1,42 @@
 import React from 'react'
-import "./ExpenseForm.css"
-import { MdSend } from 'react-icons/md'
-const ExpenseForm=()=>  {
+import { MdSend } from 'react-icons/md';
+import "./ExpenseForm.css";
 
+const ExpenseForm = ({ handleCharge, charge, edit, amount, handleAmount, handleSubmit }) => {
     return (
-      <form>
-        <div className='form-center'>
-            <div className='form-group'>
-                <label htmlFor='expense'>지출 항목</label>
-                <input 
-                type='text'
-                className='form-control'
-                id='charge'
-                name='charge'
-                placeholder='예) 렌트비'
-                />
+        <form onSubmit={handleSubmit}>
+            <div className='form-center'>
                 <div className='form-group'>
-                    <label htmlFor='amount '>비용</label>
+                    <label htmlFor='charge'>지출 항목</label>
                     <input
-                    type='number'
-                    className='form-control'
-                    id='amount'
-                    name='amount'
-                    placeholder='예) 100'/>
-
+                        type="text"
+                        className="form-control"
+                        id="charge"
+                        name="charge"
+                        value={charge}
+                        placeholder="예) 렌트비"
+                        onChange={handleCharge}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='amount'>비용</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="amount"
+                        name="amount"
+                        value={amount}
+                        placeholder="예) 100"
+                        onChange={handleAmount}
+                    />
                 </div>
             </div>
-        </div>
-        <button type='submit' className='btn'>
-            제출
-            <MdSend className='btn-icon'/>
-        </button>
-      </form>
+            <button type='submit' className='btn'>
+                {edit ? "수정" : "제출"}
+                <MdSend className='btn-icon' />
+            </button>
+        </form>
     )
-  
 }
 
 export default ExpenseForm
--- react-budget-app/src/components/ExpenseList.js
+++ react-budget-app/src/components/ExpenseList.js
@@ -1,24 +1,31 @@
 import React from 'react'
-import ExpenseItem from './ExpenseItem'
-import { MdSend } from 'react-icons/md'
-
-const ExpenseList =({handleDelete,initialExpenses}) => {
+import { MdDelete } from 'react-icons/md';
+import ExpenseItem from './ExpenseItem';
+import './ExpenseList.css';
+const ExpenseList = ({ handleDelete, expenses, handleEdit, clearItems }) => {
     return (
-      <>
-      <ul className='list'>
-        {initialExpenses.map(expense =>{
-          return (
-            <ExpenseItem expense={expense} key={expense.id} handleDelete ={handleDelete}/>
-          )
-        })}
-      </ul>
-      <button className='btn'>
-        목록 지우기
-        <MdSend className='btn-icon'/>
-      </button>
-      </>
+        <>
+            <ul className='list'>
+                {/* Expense Item */}
+                {expenses.map(expense => {
+                    return (
+                        <ExpenseItem
+                            expense={expense}
+                            key={expense.id}
+                            handleDelete={handleDelete}
+                            handleEdit={handleEdit}
+                        />
+                    )
+                })}
+            </ul>
+            {expenses.length > 0 && (
+                <button className='btn' onClick={clearItems}>
+                    목록 지우기
+                    <MdDelete className='btn-icon' />
+                </button>
+            )}
+        </>
     )
-
 }
 
 export default ExpenseList

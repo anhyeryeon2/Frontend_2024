8a99b04ff5f4258cf9ed25cef8aced5f44b2d42a
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-11-05 22:42:37 +0900

    useSelector,useDispatch

--- react-redux-app/src/App.tsx
+++ react-redux-app/src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./reducers";
+
 type Props ={
   value:any;
   onIncrement: () =>void;
   onDecrement:() =>void;
 }
 function App({value, onIncrement,onDecrement}:Props) {
+  const dispatch = useDispatch();
+  const counter = useSelector((state:RootState) =>state.counter);
+  const todos:string[] = useSelector((state:RootState)=> state.todos);
+
+  const [todoValue, setTodoValue] = useState("");
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  }
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({type:"ADD_TODO", text:todoValue})
+    setTodoValue("");
+  }
+  
   return (
     <div className="App">
      {/* Clicked : {value} times */}
@@ -13,8 +31,18 @@ function App({value, onIncrement,onDecrement}:Props) {
      <button onClick={onDecrement}>
       -
      </button>
+    <ul>
+      {todos.map((todo,index) => <li key={index}>{todo}</li>)}
+    </ul>
+     <form onSubmit={addTodo}>
+      <input type="text" value={todoValue} onChange={handleChange}/>
+      <input type="submit"/>
+
+     </form>
     </div>
   );
 }
 
 export default App;
+
+
--- react-redux-app/src/index.tsx
+++ react-redux-app/src/index.tsx
@@ -3,27 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
-import counter from "./reducers"
 import rootReducer from './reducers';
-
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 const store = createStore(rootReducer);
 
-store.dispatch({
-  type:"ADD_TODO",
-  text:"USE REDUX"
-})
-
-console.log("store에 잘 들어갔는지 확인 store.getState", store.getState())
 const render =() => root.render(
   <React.StrictMode>
-    <App 
+    <Provider store={store}>
+      <App 
     value={store.getState()} 
     onIncrement={()=> store.dispatch({type: "INCREMENT"})}
     onDecrement={()=> store.dispatch({type: "DECREMENT"})}/>
+    </Provider>
+    
   </React.StrictMode>
 );
 render();

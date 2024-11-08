83af97545d48f4c993cb3aefe6fd43be89730132
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-11-08 10:12:12 +0900

    TypeScript 변수선언

--- hacker-news-client/app.ts
+++ hacker-news-client/app.ts
@@ -1,8 +1,23 @@
-const container = document.getElementById('root');
-const ajax = new XMLHttpRequest();
+
+type Store = {
+  currentPage:number;
+  feeds:NewsFeed[];
+}
+type NewsFeed = {
+  id:number;
+  comments_count:number;
+  url:string;
+  user:string;
+  time_ago:string;
+  points:number;
+  title:string;
+  read?:boolean;
+}
+const container:HTMLElement | null = document.getElementById('root');
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const store = {
+const store:Store = {
   currentPage: 1,
   feeds: [],
 };
@@ -21,9 +36,15 @@ function makeFeeds(feeds) {
 
   return feeds;
 }
-
+function updateView(html){
+  if(container != null){
+    container.innerHTML = html;
+}else{
+  console.error("")
+}
+}
 function newsFeed() {
-  let newsFeed = store.feeds;
+  let newsFeed:NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
     <div class="bg-gray-600 min-h-screen">
@@ -80,7 +101,8 @@ function newsFeed() {
   template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
   template = template.replace('{{__next_page__}}', store.currentPage + 1);
   
-  container.innerHTML = template;
+ updateView(template);
+
 }
 
 function newsDetail() {
@@ -144,7 +166,7 @@ function newsDetail() {
     return commentString.join('');
   }
 
-  container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+  updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
 }
 
 function router() {

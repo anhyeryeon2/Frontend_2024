fcd7c9207017c2c70ed4f61dbf42038f3eb28e51
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-11-12 12:08:18 +0900

    feat:redux 인증 구현

--- react-shop-app/src/pages/RegisterPage/sign-up/SignUp.jsx
+++ react-shop-app/src/pages/RegisterPage/sign-up/SignUp.jsx
@@ -3,16 +3,23 @@ import { useNavigate } from 'react-router-dom';
 import Form from '../../../components/form/Form';
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import app from '../../../firebase';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../../store/user/user.slice';
 
 const SignUp = () => {
   const navigate = useNavigate();
   const [firebaseError, setFirebaseError] = useState("");
   const auth = getAuth(app);
+  const dispatch = useDispatch();
 
   const handleSignupAndLogin = (email, password) => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // 리덕스 스토어에 담는 로직
+        dispatch(setUser({
+          email:userCredential.user.email,
+          token:userCredential.user.refreshToken,
+          id: userCredential.user.uid
+        }))
         
         navigate('/');
       })

9d42bdf2f49c5a481ac3720abf5e103b4ffc904a
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-11-20 15:06:35 +0900

    feat:md 데이터 추출, getStaticProps 리스트 나열

--- nextjs-basic/lib/post.ts
+++ nextjs-basic/lib/post.ts
@@ -1,1 +1,33 @@
 //md파일을 데이터로 바꾸는 함수
+import fs from "fs"
+import path from "path"
+import matter from "gray-matter"
+
+const postsDirectory = path.join(process.cwd(),'posts')
+console.log("process.cwd()", process.cwd());
+console.log("postsDirectory",postsDirectory);
+
+export function getSortedPostsData(){
+    const fileNames = fs.readdirSync(postsDirectory)
+    console.log("fileName",fileNames);
+    const allPostsData = fileNames.map(fileName => {
+        const id = fileName.replace(/|.md$/,"")
+
+        const fullPath = path.join(postsDirectory,fileName)
+        const fileContents = fs.readFileSync(fullPath,"utf8")
+
+        const matterResult = matter(fileContents)
+
+        return{
+            id,
+            ...(matterResult.data as {date:string; title:string})
+        }
+    })
+    return allPostsData.sort((a,b)=>{
+        if(a.date < b.date){
+            return 1
+        }else{
+            return -1
+        }
+    })
+}
--- nextjs-basic/src/pages/index.tsx
+++ nextjs-basic/src/pages/index.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import homeStyles from "@/styles/Home.module.css";
+import { getSortedPostsData } from "../../lib/post";
+import { GetStaticProps } from "next";
 
-export default function Home() {
+export default function Home({allPostsData}:{
+  allPostsData:{
+    date:string
+    title:string
+    id:string
+  }[]
+}) {
   return (
     <div>
       <Head>
@@ -16,9 +24,26 @@ export default function Home() {
       <section className={`${homeStyles.headingMd}  ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}>BLog</h2>
         <ul className={homeStyles.list}>
-
+          {allPostsData.map(({id,date,title})=>(
+            <li className={homeStyles.listItem} key={id}>
+              <a>{title}</a>
+              <br/>
+              <small className={homeStyles.lightText}>
+                {date}
+              </small>
+            </li>
+          ))}
         </ul>
       </section>
     </div>
   );
+}
+
+export const getStaticProps:GetStaticProps = async()=>{
+  const allPostsData = getSortedPostsData()
+  return{
+    props:{
+      allPostsData
+    }
+  }
 }
